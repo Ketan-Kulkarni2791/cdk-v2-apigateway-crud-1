1733fbce54e27258a753147ae0f0ec097b718503
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-03-08 19:04:12 +0530

    create role added

--- infra/cdk/stack_blueprints/iam_construct.py
+++ infra/cdk/stack_blueprints/iam_construct.py
@@ -8,6 +8,19 @@ class IAMConstruct:
     """Class holds methods for IAM resource creation"""
 
     @staticmethod
+    def create_role(stack: Stack, config: dict, role_name: str,
+                    assumed_by: List[str]) -> iam.Role:
+        """Create role utilized by lambda, glue, step function, or the stack itself."""
+        services = list(map(lambda x: iam.ServicePrincipal(
+            f"{x}.amazonaws.com"), assumed_by))
+        return iam.Role(
+            scope=stack,
+            id=f"{config['global']['appNameShort']}-{role_name}-role-id",
+            role_name=f"{config['global']['appNameShort']}{role_name}-role",
+            assumed_by=iam.CompositePrincipal(*services)
+        )
+
+    @staticmethod
     def create_managed_policy(
             stack: Stack,
             config: dict,

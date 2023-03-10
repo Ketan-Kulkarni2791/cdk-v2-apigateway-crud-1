a3dbec35d3cd0982a8e672be76a01ded984efad6
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-03-10 20:10:07 +0530

    error fixed

--- infra/cdk/stack_blueprints/stack.py
+++ infra/cdk/stack_blueprints/stack.py
@@ -60,10 +60,9 @@ class MainProjectStack(aws_cdk.Stack):
         product_serverless_api.add_method('DELETE')
         products_serverless_api = serverless_api.root.add_resource('products')
         products_serverless_api.add_method('GET')
-    
+
         deployment = aws_apigateway.Deployment(stack, "prod", api=serverless_api)
         aws_apigateway.Stage(stack, "dev", deployment=deployment)
-
 
     @staticmethod
     def create_lambda_functions(

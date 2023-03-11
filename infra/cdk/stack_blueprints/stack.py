1579a4d709fd6bea7c2c7f24887d24bf97b3417c
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-03-11 11:38:42 +0530

    testing

--- infra/cdk/stack_blueprints/stack.py
+++ infra/cdk/stack_blueprints/stack.py
@@ -51,6 +51,7 @@ class MainProjectStack(aws_cdk.Stack):
             'serverless-api',
             handler=lambdas["placeholder_lambda"],
         )
+
         health_serverless_api = serverless_api.root.add_resource('health')
         health_serverless_api.add_method('GET')
         product_serverless_api = serverless_api.root.add_resource('product')
@@ -60,8 +61,8 @@ class MainProjectStack(aws_cdk.Stack):
         product_serverless_api.add_method('DELETE')
         products_serverless_api = serverless_api.root.add_resource('products')
         products_serverless_api.add_method('GET')
-        deployment = aws_apigateway.Deployment(stack, "serverless-api-deploy", api=serverless_api)
-        aws_apigateway.Stage(stack, "dev", deployment=deployment)
+        # deployment = aws_apigateway.Deployment(stack, "serverless-api-deploy", api=serverless_api)
+        # aws_apigateway.Stage(stack, "dev", deployment=deployment)
 
     @staticmethod
     def create_lambda_functions(
